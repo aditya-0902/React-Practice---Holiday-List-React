dd96c0476424bff6876627ae764ea4da2ff1034c
aditya-0902 <88245845+aditya-0902@users.noreply.github.com>
2021-08-13 15:16:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,36 +11,15 @@ class App extends Component {
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
-    { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
+    { name: 'Lonavala', country: 'India' }
     ]
   }
 
   render() {
    let list = this.cityList.filter((pair)=>{
-      return(pair[name]==="Goa"||pair[name]==="Amsterdam"||pair[name]==="New York"||pair[name]==="Darjeeling"||pair[name]==='Tokyo'||pair[name]==='Lonavala')
-    return (
+      return pair.country==='India'
+   })
+     return (
       <div id="main">
       <ol>
         {list.map((pair,i)=>{
@@ -52,5 +31,6 @@ class App extends Component {
       </div>
     )
   }
+}
 }
 export default App;
